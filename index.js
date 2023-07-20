8db3ca9bc36ecea3480f1646af9506466c700100
bilal-nasr <bilal.nasr2711@gmail.com>
2023-07-20 15:53:38 +0300

    2nd push

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ let letter ={
     "F":59
 } 
 
+let submit = ()=>{
+    letterToNumber();
+    getCredit();
+}
 
 let letterToNumber = ()=>{
     let letterGrade = document.getElementById("Grade").value;
@@ -68,4 +72,108 @@ let letterToNumber = ()=>{
             number = 0
     }*/
 
+}
+
+let creditValidation = (credits) =>{
+    
+    return (credits>=0 && credits<=4?  true : false);
+}
+
+let getCredit = ()=>{
+    let credits = document.getElementById("Credits");
+    if(!creditValidation(credits.value)){
+        let warning = document.getElementById("warning");
+        warning.textContent= "Credits must be an integer between 0 and 4.";
+        warning.style.display = "block";
+    }
+    else{
+        warning.style.display = "none";
+    }
+}
+
+//-------------------------------------------------------
+
+var semesterCount = 0;
+var calculateButton = document.getElementById("calculate");
+addSemester();
+function calculateGPA() {
+    calculateButton.disabled=false
+}
+
+function addSemester() {
+    let semesterContainer = document.getElementById("semester-container");
+    semesterCount++;
+    let newSemester = document.createElement("div");
+    newSemester.id = "semester";
+    newSemester.className = "semesterClass";
+    newSemester.innerHTML = `<h3>semester  ${semesterCount}</h3>`+
+                            '<button onclick="addCourse(this.parentNode)" class="btn-primary"> add course </button> '+
+                            '<button onclick="removeSemester(this.parentNode)" class="btn-primary"> remove semester </button>';
+
+    semesterContainer.appendChild(newSemester);
+    calculateButton.disabled=false;
+
+    addCourse(newSemester);
+}
+
+function removeSemester(semester){
+    semester.remove();
+    semesterCount--;
+    (semesterCount==0? calculateButton.disabled=true: calculateButton.disabled=false)
+}
+
+function addCourse(semesterDiv){
+    let newCourse = document.createElement("div");
+    newCourse.className = "Course"
+
+    let courseName = document.createElement("input");
+    courseName.type = "text";
+
+    let courseCredit = document.createElement("input");
+    courseCredit.type = "number";
+
+    let gradeSelectInput = document.createElement("select");
+    let initialOption = document.createElement("option");
+    initialOption.text = "Select Grade";
+    initialOption.disabled = true;
+    initialOption.selected = true;
+    gradeSelectInput.appendChild(initialOption);
+
+    let removeCourse = document.createElement("button");
+    removeCourse.onclick = function(){
+        removeCourseFunction(newCourse);
+    }
+
+    let grades = [
+        "A+",
+        "A",
+        "A-",
+        "B+",
+        "B",
+        "B-",
+        "C+",
+        "C",
+        "C-",
+        "D+",
+        "D",
+        "D-",
+        "F",
+    ]
+
+    for(let i = 0; i<grades.length; i++){
+        let option = document.createElement("option");
+        option.value = grades[i];
+        option.text = grades[i]
+        gradeSelectInput.appendChild(option);
+    }
+
+    newCourse.appendChild(courseName)
+    newCourse.appendChild(gradeSelectInput);
+    newCourse.appendChild(courseCredit);
+
+    semesterDiv.appendChild(newCourse);
+}
+
+function removeCourseFunction(course){
+    course.remove();
 }
